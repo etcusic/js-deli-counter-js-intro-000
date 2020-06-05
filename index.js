41c8b61afa7b5ad6a53639e6d1331066aaf17118
Eli Cusic <tntitans31@gmail.com>
2020-06-05 02:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, person){
 
 function nowServing(){
   if (katzDeliLine.length === 0){
-    return "The line is currently empty."
+    return "There is nobody waiting to be served!"
   } else {
     var person = katzDeliLine.shift
     return person

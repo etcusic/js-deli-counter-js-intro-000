e4090637ecd2d52f01050cd57a881721212843ab
Eli Cusic <tntitans31@gmail.com>
2020-06-05 02:41:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line, person){
   line.push(person)
   return `Welcome, ${person}. You are number ${line.length} in line.`
 }
+
+function nowServing(){
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else {
+    var person = katzDeliLine.shift
+    return person
+  }
+}
